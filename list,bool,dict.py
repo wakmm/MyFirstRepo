d6241153950419c38e48c350b99f81f151455e8a
wajahat wakeel <daniyalwakeel786@gmail.com>
2025-08-23 12:33:59 +0500

    Add files via upload

---
+++ list,bool,dict.py
@@ -0,0 +1,30 @@
+#list
+fruits=["apple","banana","mango"]
+print("fruits list:",fruits)
+
+fruits.append("orange")
+print("updated fruits list:",fruits)
+print("First fruit:",fruits[3])
+
+#boolean
+hungry=True
+
+if hungry>=True:
+    print("eat food")
+else:
+    print("its ok")
+
+#dorectory
+student={
+    "name":"ali", 
+         "age":29, 
+         "profession":"doctor"
+         }
+print("NAME:",student["name"])
+
+                        
+      
+
+
+
+

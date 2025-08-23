e1e6591df6fb64d2b421df8b3c592f12caf1e462
wajahat wakeel <daniyalwakeel786@gmail.com>
2025-08-23 12:05:36 +0500

    Add files via upload

---
+++ variable4.py
@@ -0,0 +1,19 @@
+#some example varaiables
+course_name = "python"
+batch_number = 3
+assignment = "variables"
+assignment_number = 1.1
+assignment_presenter = "Wajahat Wakeel"
+
+#show output by using print function
+print(f"Course Name:",course_name)
+print(f"batch number:",batch_number)
+print(f"assignment name:",assignment,"and assignment number is:",assignment_number)
+print(f"presentator name:",assignment_presenter)
+
+#checking types of values in variables
+print(f"the type of variable course_name:",type(course_name))
+print(f"the type of variable batch_number:",type(batch_number))
+print(f"the type of variable assignment:",type(assignment))
+print(f"the type of variable assignment_number:",type(assignment_number))
+print(f"the type of variable assignment_presenter:",type(assignment_presenter))
